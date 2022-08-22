284fbfd934b24f5a4c5855934752b3a394a1fb2f
Matthew Howes <howeszy@gmail.com>
2022-08-21 22:44:37 -0400

    Added error to authorize

--- lib/snfoil/crud/setup_context.rb
+++ lib/snfoil/crud/setup_context.rb
@@ -29,10 +29,13 @@ module SnFoil
 
         interval :setup
 
-        authorize do |options|
-          options.fetch(:policy) { policy }
-                 .new(entity, options[:object], **options)
-                 .send(options.fetch(:authorize) { "#{options[:action]}?" })
+        authorize do |**options|
+          authentication_policy = options.fetch(:policy) { policy }
+          authentication_action = options.fetch(:authorize) { "#{options[:action]}?" }
+          next true if authentication_policy.new(entity, options[:object], **options).send(authentication_action)
+
+          raise SnFoil::AuthorizationError,
+                "#{entity ? entity&.class : 'Unknown'}: #{entity&.id} is not allowed to #{authentication_action} on #{authentication_policy}"
         end
       end
 
